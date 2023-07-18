26654aa22be8da39a626fccd7acad3f7d775f463
Rohit <73820496+R0h1t3@users.noreply.github.com>
2023-07-18 23:38:49 +0530

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+NC="\e[0m"
+GR="\e[0;32m"
+YL="\e[0;33m"
+CY="\e[0;36m"
+
+shred -u -f Scope.ods domains.txt Scope.csv 2> /dev/null &> /dev/null
+url=$(echo -e "https://www.communicatierijk.nl$(curl -ks https://www.communicatierijk.nl/vakkennis/r/rijkswebsites/verplichte-richtlijnen/websiteregister-rijksoverheid | grep ".ods" | sed -n 's/.*href="\([^"]*\).*/\1/p')")
+
+echo -e "FILE PATH:$CY $url $NC"
+wget -O Scope.ods $url &> /dev/null
+libreoffice --convert-to csv Scope.ods --headless 2> /dev/null &> /dev/null
+cat Scope.csv | tr ',' ' ' | awk '{print $1}' | sed 's/http:\/\/\|https:\/\///g' | sed -e '1,2d' | sed 's/\/.*//' | sed '/\.\([^\.]*\.\)/s/^[^.]*\.//' | sort -u | uniq -u > domains.txt
+sleep 2s
+echo ""
+echo -e "Total number of subdomains found:$GR $(cat domains.txt | wc -l) $NC\n"
+echo -e "The list of In-Scope domains are saved in:$YL $(pwd)/domains.txt $NC\n"
+
